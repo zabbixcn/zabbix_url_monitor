844244096e251edf4cfb5ff5da468e87a4f4d990
jon <jon.kelley@rackspace.com>
2016-07-08 17:27:08 -0500

    Removing extraneous exception handling

--- url_monitor/main.py
+++ url_monitor/main.py
@@ -244,14 +244,11 @@ def main(arguements=None):
 if __name__ == "__main__":
     # do the UNIX double-fork magic, see Stevens' "Advanced 
     # Programming in the UNIX Environment" for details (ISBN 0201563177)
-    try: 
-        pid = os.fork() 
-        if pid > 0:
-            # exit first parent
-            sys.exit(0) 
-    except OSError, e: 
-        print >>sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror) 
-        sys.exit(1)
+    pid = os.fork() 
+    if pid > 0:
+        # exit first parent
+        sys.exit(0) 
+
 
     # decouple from parent environment
     os.chdir("/") 
@@ -259,15 +256,12 @@ if __name__ == "__main__":
     os.umask(0) 
 
     # do second fork
-    try: 
-        pid = os.fork() 
-        if pid > 0:
-            # exit from second parent, print eventual PID before
-            print("Daemon PID %d" % pid)
-            sys.exit(0) 
-    except OSError, e: 
-        print >>sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror) 
-        sys.exit(1) 
+    pid = os.fork() 
+    if pid > 0:
+        # exit from second parent, print eventual PID before
+        print("Daemon PID %d" % pid)
+        sys.exit(0) 
+
 
     # start the daemon main loop
     entry_point()
